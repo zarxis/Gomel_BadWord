3d2627f9862ea7255f056680885c1948a9c1b88e
zarxis <alisher1555@gmail.com>
2023-01-17 11:03:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,30 @@
 import telebot
-from telebot import  types
+from telebot import types
 import time
 
 api_token= "api"
 bot = telebot.TeleBot(api_token)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    key1 = types.KeyboardButton("Start")
-    bot.send_message(message.chat.id, "ОТВАЛИ, Я НЕ РАБОТАЮ!")
+    key1 = types.KeyboardButton("/Stat")
+    key2 = types.KeyboardButton("/ShowId")
+    markup.add(key1, key2)
+    bot.send_message(message.chat.id, "Захотел узнать как часто ты использешь BAD WORDS? Правильно, ты по адресу!", reply_markup=markup)
 
-#@bot.message_handler(commands=['Stat'])
-#def stat(message):
-#    bot.send_message(message.chat.id, "самый большой матершинник это...")
-#    n = time.sleep(5)
-#    while n < 6:
-#        bot.send_message(message.chat.id, "узнаем через n")
-#    bot.send_message(message.chat.id, "ты")
+#просто пихать будем тут функции, да я говно-кодер
+
+@bot.message_handler(commands=['Stat'])
+def stat(message):
+    bot.send_message(message.chat.id, "самый большой матершинник это...")
+    time.sleep(1)
+    bot.send_message(message.chat.id, "ты")
+
+@bot.message_handler(commands=['ShowId'])
+def my_id(message):
+    user_id = message.from_user.id
+    bot.send_message(message.chat.id, user_id)
 
 bot.infinity_polling()
