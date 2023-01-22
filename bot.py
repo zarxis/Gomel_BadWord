576fcecbd5f61852ee8779a29fb09cd659f26a7d
Rris <Darian.Frey@yandex.ru>
2023-01-22 23:30:22 +0300

    Add Fweather

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-import telebot, time, DB_CON
+import telebot, time, DB_CON, Fweather
 from telebot import types
+from pygismeteo import Gismeteo
 
 api_token= "api"
 bot = telebot.TeleBot(api_token)
@@ -10,7 +11,8 @@ def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     key1 = types.KeyboardButton("/Stat")
     key2 = types.KeyboardButton("/ShowId")
-    markup.add(key1, key2)
+    key3 = types.KeyboardButton("/Weather")
+    markup.add(key1, key2, key3)
     bot.send_message(message.chat.id, "Захотел узнать как часто ты использешь BAD WORDS? Правильно, ты по адресу!", reply_markup=markup)
 
 
@@ -21,6 +23,26 @@ def start(message):
 #    n = bot.get_me()
 #    bot.send_message(message.chat.id, n)
 
+#
+@bot.message_handler(commands=['Weather'])
+def stat(message):
+    bot.send_message(message.chat.id, "Введите название города.")
+    bot.register_next_step_handler(message, GetWeather, message.chat.id)
+    
+def GetWeather(mesInfo, mesId):
+    sityName = mesInfo.text
+    gismeteo=Gismeteo()
+    if isinstance(sityName, str):
+        search_results = gismeteo.search.by_query(sityName)
+        city_id = search_results[0].id
+        current = gismeteo.current.by_id(city_id)
+        A = str(current.temperature.air.c) + ", " + current.description.full
+        A = A + ", " + str(current.humidity.percent) + ", " + str(current.cloudiness.percent)
+        bot.send_message(mesId, sityName)
+        bot.send_message(mesId, A)
+    else:
+        bot.send_message(mesId, "ERROR")
+    
 #
 @bot.message_handler(commands=['Stat'])
 def stat(message):
@@ -34,4 +56,4 @@ def my_id(message):
     user_id = message.from_user.id
     bot.send_message(message.chat.id, user_id)
 
- bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0)
