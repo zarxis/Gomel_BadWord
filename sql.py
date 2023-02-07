d7de603707506961e14c9c3f1fd7652f3996bbd1
zarxis <alisher1555@gmail.com>
2023-02-07 17:48:32 +0300

    Update sql.py

--- sql.py
+++ sql.py
@@ -6,3 +6,4 @@ cur = con.cursor()
 def db_table_val(user_id: int, first_name: str, last_name: str,):
 	cur.execute('INSERT INTO BadWord(user_id, first_name, last_name) VALUES (?, ?, ?)', (user_id, first_name, last_name))
 	con.commit()
+	con.close()
